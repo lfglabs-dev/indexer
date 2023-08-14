ad29680928eea66e466929ab5b3f551effc97968
Thomas Marchand <thomas.marchand@tuta.io>
2023-08-14 17:25:05 +0100

    wip: detect amount

--- src/listener.ts
+++ src/listener.ts
@@ -2,10 +2,11 @@ import { hash, uint256, formatUnits } from "./deps.ts";
 import { decodeDomain } from "./utils/starknetid.ts";
 
 const DECIMALS = 18;
-// Can read from environment variables if you want to.
-// In that case, run with `--env-from-file .env` and put the following in .env:
-// TOKEN_DECIMALS=18
-// const DECIMALS = Deno.env.get('TOKEN_DECIMALS') ?? 18;
+const TRANSFER_KEY = BigInt(hash.getSelectorFromName("Transfer"));
+const STARK_UPDATE_KEY = BigInt(hash.getSelectorFromName("starknet_id_update"));
+const NAMING_CONTRACT = BigInt(
+  "0x06ac597f8116f886fa1c97a23fa4e08299975ecaf6b598873ca6792b9bbfb678"
+);
 
 export const filter = {
   header: {
@@ -15,65 +16,65 @@ export const filter = {
     {
       fromAddress:
         "0x049D36570D4e46f48e99674bd3fcc84644DdD6b96F7C741B1562B82f9e004dC7",
-      keys: [hash.getSelectorFromName("Transfer")],
+      keys: ["0x" + TRANSFER_KEY.toString(16)],
     },
     {
       fromAddress:
         "0x6ac597f8116f886fa1c97a23fa4e08299975ecaf6b598873ca6792b9bbfb678",
-      keys: [hash.getSelectorFromName("starknet_id_update")],
+      keys: ["0x" + STARK_UPDATE_KEY.toString(16)],
     },
   ],
 };
 
-const transfer_key = BigInt(hash.getSelectorFromName("Transfer"));
-const stark_update_key = BigInt(hash.getSelectorFromName("starknet_id_update"));
-
 export function decodeTransfersInBlock({ header, events }) {
   const { blockNumber, blockHash, timestamp } = header;
-
   let lastTransfer = null;
 
   const decodedEvents = events.map(({ event, receipt }) => {
     const { transactionHash } = receipt;
-    if (BigInt(event.keys[0]) === transfer_key) {
+
+    // fetching amount
+    if (BigInt(event.keys[0]) === TRANSFER_KEY) {
       const [fromAddress, toAddress, amountLow, amountHigh] = event.data;
-      const amountRaw = uint256.uint256ToBN({
-        low: amountLow,
-        high: amountHigh,
-      });
-      const amount = formatUnits(amountRaw, DECIMALS);
+      if (BigInt(toAddress) != NAMING_CONTRACT) return;
 
       lastTransfer = {
         from_address: fromAddress,
-        to_address: toAddress,
-        amount: +amount,
-        amount_raw: amountRaw.toString(),
+        amount: uint256.uint256ToBN({
+          low: amountLow,
+          high: amountHigh,
+        }),
       };
 
-      return null;
+      console.log("detected a payment of " + lastTransfer.amount);
+      return;
     }
 
-    if (BigInt(event.keys[0]) === stark_update_key) {
+    if (BigInt(event.keys[0]) === STARK_UPDATE_KEY) {
       console.log("received a domain update");
 
-      const arr_len = event.data[0];
-      let domain = decodeDomain(event.data.slice(1, 1 + arr_len));
-      return {
-        network: "starknet-goerli",
-        symbol: "ETH",
-        block_hash: blockHash,
-        block_number: +blockNumber,
-        block_timestamp: timestamp,
-        transaction_hash: transactionHash,
-        domain: domain,
-        price: lastTransfer?.amount,
-        price_raw: lastTransfer?.amount_raw,
-        from_address: lastTransfer?.from_address,
-        to_address: lastTransfer?.to_address,
+      const arr_len = Number(event.data[0]);
+      const expiry = Number(event.data[arr_len + 2]);
+
+      const output = {
+        domain: decodeDomain(
+          event.data.slice(1, 1 + arr_len).map((number) => BigInt(number))
+        ),
+        type: "purchase",
+        timestamp: new Date(timestamp),
+        price: lastTransfer?.amount.toString(),
+        payer: lastTransfer?.from_address,
+        expiry,
+        // auto
+        // sponsor
+        // sponsor_com
       };
+      console.log(output);
+      lastTransfer = null;
+      return output;
     }
 
-    return null;
+    return;
   });
 
   // Filter out null values
