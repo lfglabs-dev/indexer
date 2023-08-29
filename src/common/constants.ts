27b5af0f296792ef7063a9eeac16cc0a99e828b4
Thomas Marchand <thomas.marchand@tuta.io>
2023-08-29 17:41:04 +0100

    initial commit

---
+++ src/common/constants.ts
@@ -0,0 +1,18 @@
+import { hash } from "./deps.ts";
+
+export function formatFelt(key: bigint): string {
+  return "0x" + key.toString(16);
+}
+
+export const SELECTOR_KEYS = {
+  TRANSFER: BigInt(hash.getSelectorFromName("Transfer")),
+  USER_DATA_UPDATE: BigInt(Deno.env.get("UserDataUpdate") as string),
+};
+
+export const MONGO_CONNECTION_STRING = Deno.env.get(
+  "MONGO_CONNECTION_STRING"
+) as string;
+export const IDENTITY_CONTRACT = BigInt(
+  Deno.env.get("IDENTITY_CONTRACT") as string
+);
+export const DECIMALS = 18;
---
+++ src/id_user_data.ts
@@ -0,0 +1,48 @@
+import { Block, EventWithTransaction } from "./common/deps.ts";
+import {
+  formatFelt,
+  SELECTOR_KEYS,
+  IDENTITY_CONTRACT,
+} from "./common/constants.ts";
+
+const filter = {
+  header: { weak: true },
+  events: [
+    {
+      fromAddress: formatFelt(IDENTITY_CONTRACT),
+      keys: [formatFelt(SELECTOR_KEYS.USER_DATA_UPDATE)],
+    },
+  ],
+};
+
+export const config = {
+  streamUrl: Deno.env.get("STREAM_URL"),
+  startingBlock: Number(Deno.env.get("STARTING_BLOCK")),
+  network: "starknet",
+  filter,
+  sinkType: "mongo",
+  sinkOptions: {
+    database: "starknetid",
+    collectionName: "id_user_data",
+  },
+  entityMode: true,
+};
+
+export default function transform({ events }: Block) {
+  // const { timestamp } = header as BlockHeader;
+  // const output = events.map(({ event }: EventWithTransaction) => {
+  //   const key = BigInt(event.keys[0]);
+  //   const to = event.data[1];
+  //   const id = BigInt(event.data[2]).toString();
+  //   return {
+  //     entity: { id },
+  //     update: {
+  //       $set: {
+  //         id: id,
+  //         owner: to,
+  //         main: { $cond: [{ $not: ["$main"] }, false, "$main"] },
+  //       },
+  //     },
+  //   };
+  // });
+}
