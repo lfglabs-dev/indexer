07a9597c56b42ed4e063f2e4dc7731d4ff04ba75
Thomas Marchand <thomas.marchand@tuta.io>
2024-01-01 16:07:16 +0100

    feat: update identity verifier indexing

--- src/id_verifier_data.ts
+++ src/id_verifier_data.ts
@@ -4,8 +4,10 @@ import {
   SELECTOR_KEYS,
   IDENTITY_CONTRACT,
   MONGO_CONNECTION_STRING,
+  ID_UPGRADE_A_BLOCK,
   FINALITY,
 } from "./common/constants.ts";
+import { Event } from "https://esm.sh/v131/@apibara/indexer@0.1.2/dist/starknet/block.js";
 
 const filter = {
   header: { weak: true },
@@ -40,50 +42,106 @@ export const config = {
   },
 };
 
-export default function transform({ events }: Block) {
+export default function transform({ header, events }: Block) {
+  if (!header) {
+    console.log("missing header, unable to process", events.length, "events");
+    return;
+  }
   const output = events.map(({ event }: EventWithTransaction) => {
-    const key = BigInt(event.keys[0]);
-    switch (key) {
-      case SELECTOR_KEYS.VERIFIER_DATA_UPDATE: {
-        const id = event.data[0];
-        const field = event.data[1];
-        const data = event.data[2];
-        const verifier = event.data[3];
-        return {
-          entity: { id, field, verifier },
-          update: {
-            $set: {
-              id,
-              field,
-              data,
-              verifier,
-            },
-          },
-        };
-      }
-
-      case SELECTOR_KEYS.EXTENDED_VERIFIER_DATA_UPDATE: {
-        const id = event.data[0];
-
-        const verifier = event.data[1];
-        const field = event.data[2];
-
-        const dataLength = Number(event.data[3]);
-        const data = event.data.slice(4, 4 + dataLength);
-
-        return {
-          entity: { id, field, verifier },
-          update: {
-            $set: {
-              id,
-              field,
-              extended_data: data,
-              verifier,
-            },
-          },
-        };
-      }
+    if (Number(header.blockNumber) > ID_UPGRADE_A_BLOCK) {
+      return handling(event);
+    } else {
+      return oldHandling(event);
     }
   });
   return output;
+}
+
+function handling(event: Event) {
+  const key = BigInt(event.keys[0]);
+  switch (key) {
+    case SELECTOR_KEYS.VERIFIER_DATA_UPDATE: {
+      const id = event.keys[1];
+      const field = event.data[0];
+      const data = event.data[1];
+      const verifier = event.data[2];
+      return {
+        entity: { id, field, verifier },
+        update: {
+          $set: {
+            id,
+            field,
+            data,
+            verifier,
+          },
+        },
+      };
+    }
+
+    case SELECTOR_KEYS.EXTENDED_VERIFIER_DATA_UPDATE: {
+      const id = event.keys[1];
+
+      const verifier = event.data[0];
+      const field = event.data[1];
+
+      const dataLength = Number(event.data[2]);
+      const data = event.data.slice(3, 3 + dataLength);
+      return {
+        entity: { id, field, verifier },
+        update: {
+          $set: {
+            id,
+            field,
+            extended_data: data,
+            verifier,
+          },
+        },
+      };
+    }
+  }
+}
+
+function oldHandling(event: Event) {
+  const key = BigInt(event.keys[0]);
+  switch (key) {
+    case SELECTOR_KEYS.VERIFIER_DATA_UPDATE: {
+      const id = event.data[0];
+      const field = event.data[1];
+      const data = event.data[2];
+      const verifier = event.data[3];
+      return {
+        entity: { id, field, verifier },
+        update: {
+          $set: {
+            id,
+            field,
+            data,
+            verifier,
+          },
+        },
+      };
+    }
+
+    case SELECTOR_KEYS.EXTENDED_VERIFIER_DATA_UPDATE: {
+      const id = event.data[0];
+
+      const verifier = event.data[1];
+      const field = event.data[2];
+
+      const dataLength = Number(event.data[3]);
+      const data = event.data.slice(4, 4 + dataLength);
+
+      return {
+        entity: { id, field, verifier },
+        update: {
+          $set: {
+            id,
+            field,
+            extended_data: data,
+            verifier,
+          },
+        },
+      };
+    }
+  }
 }
