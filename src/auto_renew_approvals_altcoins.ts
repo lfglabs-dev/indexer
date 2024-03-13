0497839cf2bb03e1bc7ce94bc823f55f733a480c
Iris <iris.devillars@gmail.com>
2024-03-13 15:54:37 +0100

    fix: indexer approval altcoins

--- src/auto_renew_approvals_altcoins.ts
+++ src/auto_renew_approvals_altcoins.ts
@@ -60,30 +60,57 @@ export default function transform({ header, events }: Block) {
 
       switch (key) {
         case SELECTOR_KEYS.ON_APPROVE: {
-          const renewer = event.data[0];
-          const spender = event.data[1];
-          const allowance = uint256.uint256ToBN({
-            low: event.data[2],
-            high: event.data[3],
-          });
-          const contract = event.fromAddress;
+          if (event.keys.length === 3) {
+            const renewer = event.keys[1];
+            const spender = event.keys[2];
+            const allowance = uint256.uint256ToBN({
+              low: event.data[0],
+              high: event.data[1],
+            });
+            const contract = event.fromAddress;
 
-          if (!AUTO_RENEW_ALTCOINS_STRINGS.includes(BigInt(spender))) {
-            return;
-          }
+            if (!AUTO_RENEW_ALTCOINS_STRINGS.includes(BigInt(spender))) {
+              return;
+            }
 
-          return {
-            entity: { renewer },
-            update: [
-              {
-                $set: {
-                  renewer,
-                  allowance: "0x" + allowance.toString(16),
-                  erc20_addr: contract,
+            return {
+              entity: { renewer },
+              update: [
+                {
+                  $set: {
+                    renewer,
+                    allowance: "0x" + allowance.toString(16),
+                    erc20_addr: contract,
+                  },
                 },
-              },
-            ],
-          };
+              ],
+            };
+          } else {
+            const renewer = event.data[0];
+            const spender = event.data[1];
+            const allowance = uint256.uint256ToBN({
+              low: event.data[2],
+              high: event.data[3],
+            });
+            const contract = event.fromAddress;
+
+            if (!AUTO_RENEW_ALTCOINS_STRINGS.includes(BigInt(spender))) {
+              return;
+            }
+
+            return {
+              entity: { renewer },
+              update: [
+                {
+                  $set: {
+                    renewer,
+                    allowance: "0x" + allowance.toString(16),
+                    erc20_addr: contract,
+                  },
+                },
+              ],
+            };
+          }
         }
 
         // should not happen
